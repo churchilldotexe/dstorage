036ce1ec94a33183ed2647b91f171fce065dbfc8
churchilldotexe <churchillexe@gmail.com>
2024-05-21 14:33:11 +0800

    chore: reconfig eslint, added husky,lint-staged

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+   plugins: ["prettier-plugin-tailwindcss"],
+   printWidth: 100,
+   singleQuote: false,
+   semi: true,
+   tabWidth: 3,
+   trailingComma: "es5",
+   useTabs: false,
+   bracketSpacing: true,
+   endOfLine: "lf",
 };
 
 export default config;
